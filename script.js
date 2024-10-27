f8028af2d3a8741fe22c8e62bf298b28d3460b83
DanyloBondar <139725325+DanyloBondar@users.noreply.github.com>
2024-10-27 16:06:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function generateContent () {
   <p>Sprachtraining (coming soon)</p>
 </button>
 
-<button class="button-section-servey" style="border-radius: 100px; max-width: 200px;">
+<button class="button-section-servey" style="border-radius: 100px; max-width: 200px; margin-top: 0px; margin-left: 0px;">
     <img src="img/plus.png">
 </button>
 
