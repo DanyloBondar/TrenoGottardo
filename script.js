ff1ba025cb97c06a871851976661b062620f0850
DanyloBondar <139725325+DanyloBondar@users.noreply.github.com>
2024-10-27 15:51:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function generateContent () {
 </button>
 
 <button class="button-section-servey" style="border-radius: 100px; max-width: 200px;">
-    <img src="img/plus.png">
+    <img src="img/IMG_2143.png">
 </button>
 
 </div>`;
